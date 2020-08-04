403536a2ae530ffe1998ce302bf265993acdc317
ubuntuwinter <liuyihao18@mails.tsinghua.edu.cn>
2020-08-04 15:42:12 +0800

    Complete the structure

--- js/index.js
+++ js/index.js
@@ -4,45 +4,33 @@ $(function () {
     let idPattern = /^2[0-9]{9}$/;
     let phonePattern = /^(?:(?:13[0-9])|(?:14[5|7])|(?:15(?:[0-3]|[5-9]))|(?:18[0,5-9]))\d{8}$/
     let emailPattern = /^[A-Za-z0-9_\-]+@(?:[A-Za-z0-9_\-]+\.)+[A-Za-z]{2,4}$/;
-    let weChatPattern = /^[A-Za-z0-9_\-]+$/;
+    let WeChatPattern = /^[A-Za-z0-9_\-]+$/;
+
+    let Patterns = [namePattern, classPattern, idPattern, phonePattern, emailPattern, WeChatPattern];
+    let Promotes = ["Your name...", "Your class...", "Your student id...", "Your phone number...", "Your email...", "Your wechat..."];
+    let Names = ["姓名", "班级", "学号", "手机号", "邮箱", "微信号"];
 
     // 提交检查
     $("#form").submit(function () {
-        // 班级正则
-        if (!classPattern.test($("#class").val().trim())) {
-            alert("您输入的班级格式不正确，请重新输入");
-            return false;
-        }
-        // 学号正则
-        if (!idPattern.test($("#id").val().trim())) {
-            alert("您输入的学号格式不正确，请重新输入");
-            return false;
-        }
-        if (!phonePattern.test($("#phone").val().trim())) {
-            alert("您输入的手机号码格式不正确，请重新输入");
-            return false;
-        }
-        // Email正则
-        if (!emailPattern.test($("#email").val().trim())) {
-            alert("您输入的邮箱格式不正确，请重新输入");
-            return false;
-        }
-        // 微信号正则
-        if (!weChatPattern.test($("#WeChat").val().trim())) {
-            alert("您输入的微信号格式不正确，请重新输入");
-            return false;
-        }
+        // input输入检查
+        $("input").not("#submit").each(function (index) {
+            let $this = $(this);
+            if ($this.val().trim() === "" || $this.val().trim() === Promotes[index]) {
+                alert("请输入您的" + Names[index]);
+                return false;
+            } else if (!Patterns[index].test($this.val().trim())) {
+                alert("您输入的" + Names[index] + "格式不正确，请重新输入");
+                return false;
+            }
+        });
         // 检查第一志愿
-        if ($("#first").val() === "0") {
+        if ($("#first").val().trim() === "0") {
             alert("请选择您的第一志愿");
             return false;
         }
     });
 
     // 输入时检查以及显示提示文字
-    let Patterns = [namePattern, classPattern, idPattern, phonePattern, emailPattern, weChatPattern];
-    let Promotes = ["Your name...", "Your class...", "Your student id...", "Your phone number...", "Your email...", "Your wechat..."];
-
     $("input").not("#submit").each(function (index) {
         if ($(this).val().trim() !== Promotes[index]) {
             $(this).removeClass("promote");
@@ -77,7 +65,7 @@ $(function () {
                 $next.removeClass("right");
                 $next.addClass("wrong");
             }
-        });
+        }).keyup();
     });
 
     // 第一个下拉列表
@@ -93,7 +81,7 @@ $(function () {
             $next.removeClass("wrong");
             $next.addClass("required");
         }
-    });
+    }).change();
     $("#second,#third").change(function () {
         let $this = $(this);
         let $next = $(this).next();
@@ -106,5 +94,5 @@ $(function () {
             $next.removeClass("wrong");
             $next.addClass("optional");
         }
-    })
+    }).change();
 })
